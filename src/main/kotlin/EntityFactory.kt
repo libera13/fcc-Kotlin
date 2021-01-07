506fe971c6a181f621db0ea8b352e0f3ff144cf8
e.libera <e.libera@pixel.com.pl>
2021-01-07 15:55:26 +0100

    sealed class

--- src/main/kotlin/EntityFactory.kt
+++ src/main/kotlin/EntityFactory.kt
@@ -1,22 +1,59 @@
-interface IdProvider {
-    fun getId() : String
+import java.util.*
+
+enum class EntityType {
+    HELP, EASY, MEDIUM, HARD;
+    fun getFormattedName() = name.toLowerCase().capitalize()
 }
 
-class Entity private constructor(val id: String) {
-    companion object : IdProvider {
-        const val id = "id"
-
-        override fun getId() : String {
-            return "id 232232"
-        }
-
-        fun create() = Entity(getId())
+object EntityFactory {
+   fun create (type: EntityType) : Entity {
+       val id = UUID.randomUUID().toString()
+       val name = when (type) {
+           EntityType.EASY -> type.getFormattedName()
+           EntityType.MEDIUM ->  type.getFormattedName()
+           EntityType.HARD ->  type.getFormattedName()
+           EntityType.HELP ->  type.getFormattedName()
+       }
+       return when(type) {
+           EntityType.EASY -> Entity.EASY(id, name)
+           EntityType.MEDIUM -> Entity.MEDIUM(id, name)
+           EntityType.HARD -> Entity.HARD(id, name, 1.0f)
+           EntityType.HELP -> Entity.Help
+       }
+   }
+}
+sealed class Entity() {
+    object Help : Entity() {
+        val name = "help"
     }
+    data class EASY(val id: String, val name: String): Entity() {
+        fun makeMeFun() {
+            println("hahaah")
+        }
+    }
+    data class MEDIUM(val id: String, val name: String): Entity()
+    data class HARD(val id: String, val name: String, val multplier: Float): Entity()
 }
 
+fun Entity.MEDIUM.printInfo() {
+    println("hahaah medium")
+}
 
 fun main() {
-    val entity = Entity.create()
-    println(Entity.id)
+    val entity : Entity = EntityFactory.create(EntityType.MEDIUM)
+    var msg = when (entity) {
+        Entity.Help -> 2
+        is Entity.EASY -> 1
+        is Entity.MEDIUM -> 3
+        is Entity.HARD -> 4
+    }
+    if (entity is Entity.MEDIUM) {
+        entity.printInfo()
+    }
+
+    Entity.MEDIUM("id", "name").printInfo()
+
+    println(msg)
+//    println(entity.name)
 
 }
